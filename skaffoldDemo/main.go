19670e157dca7b46f36ab48ee47ab4107c638335
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-01-28 18:27:24 +0800

    [email]添加docker env
    Signed-off-by: zengqiang.fang <zengqiang.fang@thoughtworks.com>

--- skaffoldDemo/main.go
+++ skaffoldDemo/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	for {
+		fmt.Println("RUN_TIME:", os.Getenv("RUN_TIME"))
 		fmt.Println("Hello world!")
 
 		time.Sleep(time.Second * 1)
