d6184401f868262f3e8e530ff193dce8429201ad
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-03-16 21:10:33 +0800

    [email]添加日志

--- skaffoldDemo/rxjavademo/src/main/java/com/example/javademo/HelleController.java
+++ skaffoldDemo/rxjavademo/src/main/java/com/example/javademo/HelleController.java
@@ -3,11 +3,13 @@ package com.example.javademo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class HelleController {
     @GetMapping("hello")
     public Mono<String> hello(){
-        return Mono.just("hello mono!");
+        return Mono.just("hello mono!").log();
     }
 }
